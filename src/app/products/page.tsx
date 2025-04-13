b2679d4a102ec516bc2fee53f813b85df92afaf8
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2025-04-13 14:04:48 +0530

    verbose logs

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,23 @@ import urlMap from '@/lib/endpoint';
 export default async function Products() {
   const url = `${urlMap.getProducts()}`;
   console.log('Fetching products from URL:', url);
-  const response = await fetch(url, { cache: 'no-store' });
-  const products = await response.json();
+
+  try {
+    const response = await fetch(url, { cache: 'no-store' });
+
+    if (!response.ok) {
+      console.error(
+        `Failed to fetch products. Status: ${response.status}, StatusText: ${response.statusText}`,
+      );
+      throw new Error('Failed to fetch products');
+    }
+
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.error('An error occurred while fetching products:', error);
+    throw error;
+  }
 
   return (
     <>
