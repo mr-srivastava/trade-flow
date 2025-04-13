5c626a985515b0c2216226d9cbcfe66ffd7ee65e
Aadarsh Srivastava <aadarsh.srivastava16@gmail.com>
2025-04-13 13:57:08 +0530

    VERCEL URL added

--- src/lib/endpoint.ts
+++ src/lib/endpoint.ts
@@ -3,7 +3,9 @@ interface UrlMap {
   getProduct: (productId: string) => string;
 }
 
-const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
 
 const urlMap: UrlMap = {
   getProducts: () => `${baseUrl}/api/products`,
